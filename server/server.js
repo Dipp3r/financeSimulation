153d1ed40e3f49572521dc47dee598ff5c0267fd
Dipp3r <aruneesh4@gmail.com>
2023-05-16 21:46:06 +0530

    adding apis to server.js as maxListener limit has been reached in queries.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const EventEmitter = require("events");
 const bodyParser = require("body-parser");
 const app = express();
 const port = 3003;
 const db = require("./queries");
 const cors = require("cors");
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "finance",
+  password: "arun",
+  port: 5432,
+});
 
 //middleware
 app.use(cors());
@@ -30,6 +40,19 @@ app.delete("/removeUser",db.removeUser);
 app.put("/assignrole",db.alterRole);
 app.post("/login/:id", db.addUser);
 app.get("/portfolio/:id",db.getChart);
+
+app.get("/team/:id",
+async (req,res)=>{
+  const groupid = Number.parseInt(req.params.id);
+  try {
+    const players = await pool.query('SELECT userid,name,mobile,role FROM users WHERE groupid = $1',[groupid]);
+    res.status(200).send(players.rows);
+  } catch (error) {
+    console.log("Error: " + error.message);
+  }
+}
+);
+
 
 
 app.listen(port, () => {
