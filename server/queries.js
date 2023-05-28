bf70aaec8fea1280ef42c1ec1069657587f06b51
Dipp3r <aruneesh4@gmail.com>
2023-05-28 17:07:08 +0530

    restricted creation of groups and session w/o name

--- server/queries.js
+++ server/queries.js
@@ -32,50 +32,41 @@ const test = async (req, res) => {
 const addSession = async (request, response) => {
   let id = Math.floor(100000 + Math.random() * 900000);
   const [title] = Object.values(request.body);
-  try {
-    await pool.query(
-      "INSERT INTO session(sessionid,title,excelLink,time_created) VALUES($1,$2,$3,$4)",
-      [id, title, "",new Date()]
-    );
-    response.status(200).send({ status: true });
-  } catch (error) {
-    console.log("Error: " + error.message);
-    response.status(500).send("Error");
+  if(title.length > 0){
+    try {
+      await pool.query(
+        "INSERT INTO session(sessionid,title,excelLink,time_created) VALUES($1,$2,$3,$4)",
+        [id, title, "",new Date()]
+      );
+      response.status(200).send({ status: true });
+    } catch (error) {
+      console.log("Error: " + error.message);
+      response.status(500).send("Error");
+    }
+  } else{
+    response.status(200).send({status:true});
   }
-  console.log(request.body);
 };
 
 const addGroup = async (request, response) => {
   let id = Math.floor(100000 + Math.random() * 900000);
   const { name, limit, sessionid } = request.body;
-
-  try {
-    // Check if the limit is a valid integer
-    if (!Number.isInteger(limit)) {
-      throw new Error("Invalid limit. Please provide a valid integer value.");
+  if(name.length>0){
+    try {
+      if (!Number.isInteger(limit)) {
+        throw new Error("Invalid limit. Please provide a valid integer value.");
+      }
+      await pool.query(
+        'INSERT INTO "group"(groupid, name, _limit, networth, stocks, commodities, cash, mutual_funds, sessionid, players, star,time_created) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)',
+        [id, name, limit, 0, 0, 0, 0, 0, sessionid, 0, 0,new Date()]
+      );
+      response.status(200).send({ status: true });
+    } catch (error) {
+      console.log(error);
+      response.status(400).send("Error: " + error.message);
     }
-
-    await pool.query(
-      'INSERT INTO "group"(groupid, name, _limit, networth, stocks, commodities, cash, mutual_funds, sessionid, players, star,time_created) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)',
-      [id, name, limit, 0, 0, 0, 0, 0, sessionid, 0, 0,new Date()]
-    );
-
-    // const group_count = await pool.query(
-    //   "SELECT groups FROM session WHERE sessionid = $1",
-    //   [sessionid]
-    // );
-    // const [num] = Object.values(group_count.rows[0]);
-    // const new_count = Number.parseInt(num) + 1;
-
-    // await pool.query(
-    //   "UPDATE session SET groups = $1 WHERE sessionid = $2 RETURNING *",
-    //   [new_count, sessionid]
-    // );
-
+  }else{
     response.status(200).send({ status: true });
-  } catch (error) {
-    console.log(error);
-    response.status(400).send("Error: " + error.message);
   }
 };
 
