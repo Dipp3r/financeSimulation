a52794da2e0750073f364c7ee7fbf4383239f4aa
Dipp3r <aruneesh4@gmail.com>
2023-05-30 15:20:35 +0530

    line added below every asset

--- public/src/components/assestsComp.jsx
+++ public/src/components/assestsComp.jsx
@@ -16,22 +16,25 @@ export default class AssetsComp extends React.Component {
   }
   render() {
     return (
-      <div className="row">
-        <p id="name">{this.state.name}</p>
-        <div>
-          <p>₹{this.state.singlePrice}</p>
+      <div>
+        <div className="row">
+          <p id="name">{this.state.name}</p>
           <div>
-            <img src={gain} alt="gain" />
-            <p className="gain">{this.state.singlePercent}%</p>
+            <p>₹{this.state.singlePrice}</p>
+            <div>
+              <img src={gain} alt="gain" />
+              <p className="gain">{this.state.singlePercent}%</p>
+            </div>
+          </div>
+          <div>
+            <p>₹{this.state.totalPrice}</p>
+            <div>
+              <img src={loss} alt="loss" />
+              <p className="loss">₹{this.state.changedTotalPrice}</p>
+            </div>
           </div>
         </div>
-        <div>
-          <p>₹{this.state.totalPrice}</p>
-          <div>
-            <img src={loss} alt="loss" />
-            <p className="loss">₹{this.state.changedTotalPrice}</p>
-          </div>
-        </div>
+        <hr />
       </div>
     );
   }
